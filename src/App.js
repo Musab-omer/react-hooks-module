5ef8ea4ec10bbb96479e8de71bdd0832024d708f
musab-omer <mosab.katry@gmail.com>
2023-06-19 21:32:32 +0300

    imported components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
+import CarSelector from './component/pages/CarSelector';
+import MessageCard from './component/pages/MessageCard';
+import SMSApp from './component/pages/SMSApp';
+import UserRegistration from './component/pages/UserReagistration';
 
 
 let App=()=> {
   return (
     <div className="App">
        <MainNavbar/>
-      
+      {/* <UserRegistration/> */}
+      {/* <CarSelector/> */}
+      <MessageCard/>
+      {/* <SMSApp/> */}
     </div>
   );
 }
