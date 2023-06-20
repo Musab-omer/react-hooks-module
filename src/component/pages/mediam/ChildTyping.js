e73015c9e8ab7bb2465ca0edcbf1b349e69ebcc1
musab-omer <mosab.katry@gmail.com>
2023-06-20 06:23:45 +0300

    Parent & Child Typing React Hooks
    used Functional based component

--- src/component/pages/mediam/ChildTyping.js
+++ src/component/pages/mediam/ChildTyping.js
@@ -1,50 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ChildTyping extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            chMessage: ''
-        }
+let ChildTyping = (props) => {
+    let [Child, setChild] = useState({
+        chMessage: ''
+    })
 
+    let typingMessage = (event) => {
+        setChild({
+            chMessage:event.target.value
+        })
+        props.resiveMessage(event.target.value);
     }
-    typingMessage = (event) => {
-        this.setState(
-            {
-                chMessage: event.target.value
-            }
-        );
-        this.props.resiveMessage(event.target.value);
-    }
+    return (
+        <React.Fragment>
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header bg-danger text-white">
+                                <p className="h4">Child Coponent</p>
+                            </div>
+                            <div className="card-body bg-light">
 
-    render() {
-        return (
-            <React.Fragment>
-                <div className="container mt-3">
-                    <div className="row">
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-header bg-secondary text-white">
-                                    <p className="h4">Child Coponent</p>
-                                </div>
-                                <div className="card-body bg-light">
-                                    
-                                    <form>
-                                        <div className="form-group">
-                                            <input type="text" className="form-control"
-                                                value={this.state.message}
-                                                onChange={this.typingMessage}
-                                                placeholder="type here..." />
-                                        </div>
-                                    </form>
-                                    <p className="h4">From Parent : {this.props.chMessage}</p>
-                                </div>
+                                <form>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control"
+                                            value={Child.chMessage}
+                                            onChange={typingMessage}
+                                            placeholder="type here..." />
+                                    </div>
+                                </form>
+                                <p className="h4">From Parent : {props.chMessage}</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </React.Fragment>
-        );
-    }
+            </div>
+        </React.Fragment>
+    )
 }
-export default ChildTyping;
+export default ChildTyping
--- src/component/pages/mediam/ParentTyping.js
+++ src/component/pages/mediam/ParentTyping.js
@@ -1,63 +1,54 @@
-import React from "react";
-import ChildCoponent from "./ChildTyping";
+import React, { useState } from "react";
 import ChildTyping from "./ChildTyping";
 
+let ParentType = () => {
+    let [Parent, setParent] = useState({
+        pMessage: '',
+        messageFromChild: ''
+    })
 
-class ParentTyping extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-           pMessage: '',
-            messageFromChild: ''
-        }
+    let typingMessage = (event) => {
+        setParent({
+            ...Parent,
+            pMessage:event.target.value
+        })
     }
 
-    typingMessage = (event) => {
-        this.setState(
-            {
-                //...this.state,
-                pMessage: event.target.value
-            }
-        );
+    //get message from child 
+    let getMessage = (msg) => {
+        setParent({
+            ...Parent,
+            messageFromChild:msg
+        })
     }
 
-    getMessage = (msg) => {
-        this.setState(
-            {
-                ...this.state,
-                messageFromChild: msg
-            }
-        );
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <div className="container mt-3">
-                    <div className="row">
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-header bg-secondary text-white">
-                                    <p className="h4">ParentCoponent</p>
-                                </div>
-                                <div className="card-body orange">
-                                    <form>
-                                        <div className="form-group">
-                                            <input type="text" className="form-control" value={this.state.message}
-                                                onChange={this.typingMessage}
-                                                placeholder="type here.." />
-                                        </div>
-                                    </form>
-                                    <p className="h4">From Child : {this.state.messageFromChild}</p>
-                                    <ChildTyping chMessage={this.state.pMessage} resiveMessage={this.getMessage}/>
-                                </div>
+    return (
+        <React.Fragment>
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header bg-warning text-white">
+                                <p className="h4">Parent Coponent</p>
+                            </div>
+                            <div className="card-body orange">
+                                <form>
+                                    <div className="mb-3">
+                                        <input type="text" className="form-control" value={Parent.pMessage}
+                                            onChange={typingMessage}
+                                            placeholder="type here.." />
+                                    </div>
+                                </form>
+                                <p className="h4">From Child : {Parent.messageFromChild}</p>
+                                <ChildTyping chMessage={Parent.pMessage} resiveMessage={getMessage} />
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
-            </React.Fragment>
-        );
-    }
+        </React.Fragment>
+    )
 }
-export default ParentTyping;
+export default ParentType
+
