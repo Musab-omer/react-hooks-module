a4edbf02b6e5220bfa3cbcf31942d6ba3d0a9563
musab-omer <mosab.katry@gmail.com>
2023-06-21 10:40:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
 
-
-
-
 let App=()=> {
   return (
     <div className="App">
        <MainNavbar/>
-   
+      
     </div>
   );
 }
