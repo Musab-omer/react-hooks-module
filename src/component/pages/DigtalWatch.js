a21dc954dae0bbd0fb2bac685c06937280e1754e
musab-omer <mosab.katry@gmail.com>
2023-06-21 11:42:40 +0300

    DigtalWatch used React Hooks
    used functionl base component and useEffect hooks

--- src/component/pages/DigtalWatch.js
+++ src/component/pages/DigtalWatch.js
@@ -1,33 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 
-class DigtalWatch extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            currentTime:new Date().toLocaleTimeString()
-        }
-    }
-
-    //executed when component is fully loadded on the DMO
-    componentDidMount =()=>{
-       this.time= setInterval(e=>{
-            this.setState({
-                currentTime:new Date().toLocaleTimeString()
-            })
-        },1000)
-    }
-
-    //executed when component is removed from the DOM
-    componentWillUnmount =()=>{
-        clearInterval(this.time);
-    }
+let DigtalWatch = () => {
+    let[currentTime,setCurrentTime]=useState(new Date().toLocaleTimeString())
     
-    render() {
-        return (
-            <React.Fragment>
-                <h1>{this.state.currentTime}</h1>
-            </React.Fragment>
-        )
-    }
+    //when component is fully loaded on DOM
+    useEffect(() => {
+        //componentDidMount
+        let timer = setInterval(() => {
+            setCurrentTime(new Date().toLocaleTimeString())
+        }, 1000);
+
+        //when the component is removed from DOM
+        return () => {
+            //componentWillUnmount
+            clearInterval(timer);
+        }
+    })
+    return (
+        <React.Fragment>
+
+            <div className='container mt-5'>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='card w-50 m-auto text-center'>
+                            <div className='card-header bg-warning'>
+                                <p className='h4'>Digtal Watch</p>
+                            </div>
+                            <div className='card-body'>
+                                <p className='h2'>{currentTime}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
 }
-export default DigtalWatch;
+export default DigtalWatch
