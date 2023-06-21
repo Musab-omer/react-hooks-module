e0a11d9f7a8978ec34f56d6ca0ab9ecdcab2ff3d
musab-omer <mosab.katry@gmail.com>
2023-06-21 14:30:06 +0300

    Props drilling react hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 
+import { useState } from 'react';
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
 import DigtalWatch from './component/pages/DigtalWatch';
+import Acomponent from './component/pages/propDrilling/Acomponent';
 
-let App=()=> {
+let App = () => {
+  let [personalInfo, setPersonalnfo] = useState({
+    firstName: 'Musab',
+    lastName: 'Omer',
+    gender: 'Male'
+  })
   return (
     <div className="App">
-       <MainNavbar/>
-      <DigtalWatch/>
+      <MainNavbar />
+      {/* <DigtalWatch/> */}
+
+      {/* ----------- prop drilling example  */}
+      <div className='container mt-3'>
+        <div className='row'>
+          <div className='col'>
+            <div className='card'>
+              <div className='card-body bg-primary'>
+                <p className='h4'>App Component</p>
+                <pre>{JSON.stringify(personalInfo)}</pre>
+                <Acomponent resivePersonalInfo={personalInfo}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
