85e024dda94b3802224fc7f35fb5d089102deb6b
musab-omer <mosab.katry@gmail.com>
2023-06-21 11:41:41 +0300

    imported digtal watch component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
+import DigtalWatch from './component/pages/DigtalWatch';
 
 let App=()=> {
   return (
     <div className="App">
        <MainNavbar/>
-      
+      <DigtalWatch/>
     </div>
   );
 }
