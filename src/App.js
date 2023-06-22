5a6e69e9f55130d8c9d5ddbb6509b89f05faf14e
musab-omer <mosab.katry@gmail.com>
2023-06-22 13:04:25 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
-import CustomersCards from './component/pages/CustomersCards';
-import UsersList from './component/pages/UsersList';
 
 let App = () => {
   return (
     <div className="App">
       <MainNavbar />
-      {/* <UsersList/> */}
-      <CustomersCards/>
+      
     </div>
   );
 }
