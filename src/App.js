d175c788fe6085009d01dd3063d7e0b208a22ad0
musab-omer <mosab.katry@gmail.com>
2023-06-22 18:23:37 +0300

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
-import ContactApp from './component/contact-app/ContactApp';
-
 let App = () => {
   return (
     <div className="App">
       <MainNavbar />
-      <ContactApp/>
     </div>
   );
 }
