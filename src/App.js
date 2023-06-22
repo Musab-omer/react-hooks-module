241459a6ab2eda0d1ca2ccc9448bd0b270f176a0
musab-omer <mosab.katry@gmail.com>
2023-06-22 17:35:52 +0300

    imported Contact app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
+import ContactApp from './component/contact-app/ContactApp';
 
 let App = () => {
   return (
     <div className="App">
       <MainNavbar />
-      
+      <ContactApp/>
     </div>
   );
 }
