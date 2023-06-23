5fbcd07a4d38905eb6b461cd7a3ebed93a69aa52
musab-omer <mosab.katry@gmail.com>
2023-06-24 02:16:50 +0300

    Conferted to React Hooks
    used funcational component

--- src/component/github/github-profile/GithubProfileDetails.js
+++ src/component/github/github-profile/GithubProfileDetails.js
@@ -1,33 +1,26 @@
 import React from "react";
 
-class GithubProfileDetails extends React.Component{
-    constructor(props) {
-        super(props);
-        
-    }
+let GithubProfileDetails=(props)=>{
 
-    render() {
-        let{name,location,email,company,blog,created_at,url,followers,following,public_repos,public_gists,html_url}=this.props.profile;
-        return (
-            <React.Fragment>
+    let{name,location,email,company,blog,created_at,url,followers,following,public_repos,public_gists,html_url}=props.profile;
+    return(
+        <React.Fragment>
                <div className="pb-3">
-               <span className="badge badge-pill badge-info mr-2">{followers} Followers</span>
-               <span className="badge badge-pill badge-success mr-2">{public_repos} Repos</span>
-               <span className="badge badge-pill badge-danger mr-2">{public_gists} Gists</span>
+               <span className="badge badge-pill badge-info mx-2">{followers} Followers</span>
+               <span className="badge badge-pill badge-success mx-2">{public_repos} Repos</span>
+               <span className="badge badge-pill badge-danger mx-2">{public_gists} Gists</span>
                <span className="badge badge-pill badge-warning">{following} Following</span>
                </div>
-               <ul className="list-group">
-                <li className="list-group-item bg-secondary text-white">User Name : {name}</li>
-                <li className="list-group-item">Location : {location}</li>
-                <li className="list-group-item">Email : {email}</li>
-                <li className="list-group-item">Compnay {company}: </li>
-                <li className="list-group-item">Blog : {blog}</li>
-                <li className="list-group-item">Member Since : {created_at}</li>
-                <li className="list-group-item">Profile URL : <a href={html_url} target="_blank">{url}</a> </li>
+               <ul className="list-group list-group-flush">
+                <li className="list-group-item bg-secondary text-white"><span className="fw-bold">User Name :</span> {name}</li>
+                <li className="list-group-item"><span className="fw-bold">Location :</span> {location}</li>
+                <li className="list-group-item"><span className="fw-bold">Email :</span> {email}</li>
+                <li className="list-group-item"><span className="fw-bold">Compnay :</span> {company}: </li>
+                <li className="list-group-item"><span className="fw-bold">Blog :</span> {blog}</li>
+                <li className="list-group-item"><span className="fw-bold">Member Since :</span> {created_at}</li>
+                <li className="list-group-item"><span className="fw-bold">Profile URL :</span> <a href={html_url} target="_blank">{url}</a> </li>
                </ul>
             </React.Fragment>
-        );
-    }
-    
+    )
 }
-export default GithubProfileDetails;
+export default GithubProfileDetails
