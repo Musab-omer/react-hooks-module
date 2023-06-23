290254709da4360d6d2482947a48b86b6f2be0b7
musab-omer <mosab.katry@gmail.com>
2023-06-24 01:37:54 +0300

    Reafactor the code

--- src/component/github/GithubSearchApp.js
+++ src/component/github/GithubSearchApp.js
@@ -2,20 +2,12 @@ import React, { useState } from "react";
 import GitHubSearch from './github-profile/GithubSearch';
 
 let GithubSearchApp =()=>{
-
-    let[userProfile,setUserProfile]=useState({});
-    let[userRepos,setUserRepos]=useState([]);
-
-    let githubUserDetails=(userProfile,userRepos)=>{
-        setUserProfile(userProfile);
-        setUserRepos(userRepos);
-    }
     return(
         <React.Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <GitHubSearch getData={githubUserDetails}/>
+                        <GitHubSearch />
                     </div>
                 </div>
             </div>
