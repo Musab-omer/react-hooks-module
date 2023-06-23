5a7c827f4576d2ade1f80d1748e24e93d302c266
musab-omer <mosab.katry@gmail.com>
2023-06-24 01:30:52 +0300

    Converted to React Hooks
    used funcational component ,useState hook to manage state and axios to get data from github server

--- src/component/github/github-profile/GithubSearch.js
+++ src/component/github/github-profile/GithubSearch.js
@@ -1,117 +1,95 @@
 import axios from "axios";
-// import { error, event } from "jquery";
-import React from "react";
+import React, { useState } from "react";
 import { CLIENT_ID, CLIENT_SECRET } from "../credentials/GithubCredentialse";
 import GithubProfileCard from "./GithubProfileCard";
 import GithubProfileDetails from "./GithubProfileDetails";
 import GithubRepos from "../github-repo/GithubRepos"
 
-class GithubSearch extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            profileUserName: '',
-            githubProfile: {},
-            githubRepos: [],
-            errorMessage: ''
+let GithubSearch = () => {
 
-        }
+    let [profileUserName, setprofileUserName] = useState('');
+    let [githubProfile, setGithubProfile] = useState({});
+    let [githubRepos, setGithubRepos] = useState([]);
+    let [errorMessage, setErrorMessage] = useState('');
+
+    // serach user profile
+    let submitSearchUser = (e) => {
+        e.preventDefault();
+        searchUserProfile(profileUserName);
+        searchRepos(profileUserName);
     }
 
-    updateState = event => this.setState({ ...this.state, profileUserName: event.target.value });
-
-    //featch data from github server 
-    submitSearchUser = (event) => {
-        event.preventDefault();
-        this.searchUserProfile(this.state.profileUserName);
-        this.searchRepos(this.state.profileUserName);
-        //alert(this.state.githubProfile);
-        //this.props.getData(this.state.githubProfile,this.state.githubRepos);
+    let searchUserProfile = (userName) => {
+        let profileUrl = `https://api.github.com/users/${userName}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+        axios.get(profileUrl).then((response => {
+            setGithubProfile(response.data);
+        })).catch((error) => {
+            console.error(error.message);
+            setErrorMessage(error.message);
+        })
     }
 
-    // search user profile
-    searchUserProfile = (githubUserName) => {
-        let profileUrl = `https://api.github.com/users/${githubUserName}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
-        //let reposUrl='https://api.github.com/users/musab-omer/repos';
-        axios.get(profileUrl).then(response => {
-            this.setState(
-                {
-                    ...this.state,
-                    githubProfile: response.data
-                }
-            );
-        }).catch(error => {
-            this.setState({
-                ...this.state,
-                errorMessage: error.message
-            });
-        });
+    // search user reposotry
+    let searchRepos = (userName) => {
+        let repoUrl = `https://api.github.com/users/${userName}/repos?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+        axios.get(repoUrl).then((response) => {
+            setGithubRepos(response.data);
+        }).catch((error) => {
+            console.error(error.message);
+            setErrorMessage(error.message);
+        })
     }
 
-    // Search user repos
-    searchRepos = (userName) => {
-        let reposUrl = `https://api.github.com/users/${userName}/repos?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
-        axios.get(reposUrl).then(response => {
-            this.setState({
-                ...this.state,
-                githubRepos: response.data
-            });
-        }).catch(error => {
-            this.setState({
-                ...this.state,
-                errorMessage: error.message
-            });
-        });
-    }
-    render() {
-        return (
-            <React.Fragment>
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            {/* User Profile <pre>{JSON.stringify(this.state.githubProfile)}</pre>
-                            User repos <pre>{JSON.stringify(this.state.githubRepos)}</pre> */}
-                            <form>
-                                <div className="form-group form-inline">
-                                    <input type="text" id="txtProfileName" value={this.profileUserName}
-                                        onChange={this.updateState}
-                                        placeholder="GitHub User Name" className="form-control" />
-                                    <input type="submit" value="search" className="btn btn-secondary btn-md" onClick={this.submitSearchUser} />
-
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-4">
-                                {
-                                    Object.keys(this.state.githubProfile).length>0?
-                                    <React.Fragment>
-                                        <GithubProfileCard profile={this.state.githubProfile}/>
-                                    </React.Fragment>:null
-                                }
+    return (
+        <React.Fragment>
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col">
+                        <form className="row">
+                            <div className="col-auto">
+                                <input type="text" className="form-control"
+                                    value={profileUserName}
+                                    onChange={e=>{setprofileUserName(e.target.value)}}
+                                    placeholder="Github User" />
                             </div>
-                            <div className="col-md-8">
-                                {
-                                   Object.keys(this.state.githubProfile).length>0?
-                                    <React.Fragment>
-                                        <GithubProfileDetails profile={this.state.githubProfile}/>
-                                    </React.Fragment>:null
-                                }
-                                
+                            <div className="col-auto">
+                                <input type="submit" value="Search" onClick={submitSearchUser} className="btn btn-info" />
                             </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col">
-                            <GithubRepos reposDetails={this.state.githubRepos}/>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
-            </React.Fragment>
-        );
-    }
+                <div className="row mt-5">
+                    <div className="col-md-4">
+                        {
+                            Object.keys(githubProfile).length > 0 ?
+                                <React.Fragment>
+                                    <GithubProfileCard profile={githubProfile} />
+                                </React.Fragment> : null
 
+                        }
+                    </div>
+                    <div className="col-md-8">
+                        {
+                            Object.keys(githubProfile).length > 0 ?
+                                <React.Fragment>
+                                    <GithubProfileDetails profile={githubProfile} />
+                                </React.Fragment> : null
+
+                        }
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        {
+                            githubRepos.length>0?
+                            <React.Fragment>
+                                <GithubRepos reposDetails={githubRepos} />
+                            </React.Fragment>:null
+                        }
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
 }
-export default GithubSearch;
+export default GithubSearch
