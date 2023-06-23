f58d26fa9f61aad479504dfe90e86f98384eaa34
musab-omer <mosab.katry@gmail.com>
2023-06-24 00:29:19 +0300

    Converted to React Hooks
    used functional based component

--- src/component/github/GithubSearchApp.js
+++ src/component/github/GithubSearchApp.js
@@ -1,42 +1,25 @@
-import React from "react";
-import GitHubSearch from './github-profile/GithubSearch'
-import GitHubProfile from './github-profile/GithubProfile';
-import GithubRepos from './github-repo/GithubRepos'
+import React, { useState } from "react";
+import GitHubSearch from './github-profile/GithubSearch';
 
-class GithubSearchApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            userProfile:{},
-            userRepos:[]
-        }
+let GithubSearchApp =()=>{
 
+    let[userProfile,setUserProfile]=useState({});
+    let[userRepos,setUserRepos]=useState([]);
+
+    let githubUserDetails=(userProfile,userRepos)=>{
+        setUserProfile(userProfile);
+        setUserRepos(userRepos);
     }
-
-    githubUserDetails=(userProfile,userRepos)=>{
-        this.setState({
-            ...this.state,
-            userProfile:userProfile,
-            userRepos:userRepos
-        })
-    }
-    render() {
-        return (
-            <React.Fragment>
-                <div className="container mt-3">
-                    <div className="row">
-                        <div className="col">
-                            <GitHubSearch getData={this.githubUserDetails}/>
-                            {/* <GitHubProfile profileDetails={this.state.userProfile}/> */}
-                            {/* <GithubRepos reposDetails={this.state.userRepos}/> */}
-                            {/* User Profile <pre>{JSON.stringify(this.state.userProfile)}</pre>
-                            User repos <pre>{JSON.stringify(this.state.userRepos)}</pre> */}
-                        </div>
+    return(
+        <React.Fragment>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <GitHubSearch getData={githubUserDetails}/>
                     </div>
                 </div>
-            </React.Fragment>
-        );
-    }
-
+            </div>
+        </React.Fragment>
+    )
 }
-export default GithubSearchApp;
+export default GithubSearchApp
