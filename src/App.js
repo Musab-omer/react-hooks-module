f41b30c22eae893a5e34d014994f4d4564bb3469
musab-omer <mosab.katry@gmail.com>
2023-06-24 15:37:43 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
-import GithubSearchApp from './component/github/GithubSearchApp'
+
 let App = () => {
   return (
     <div className="App">
       <MainNavbar />
-      <GithubSearchApp/>
+   
     </div>
   );
 }
