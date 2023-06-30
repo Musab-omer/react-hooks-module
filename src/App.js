ee62e0d475c08ee3a30f7aefb6d453854d22aa1a
musab-omer <mosab.katry@gmail.com>
2023-07-01 00:18:15 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
-import Home from './component/views/Home';
-import About from './component/views/About';
-import EmployeesList from './component/views/employees/EmployeesList';
-import EmployeesDetails from './component/views/employees/EmployeeDetails';
-import Stocks from './component/views/stocks/Stocks';
-import Contact from './component/views/Contact';
-import StockDetails from './component/views/stocks/StockDetails';
+
 
 let App = () => {
   return (
@@ -17,13 +11,7 @@ let App = () => {
       <BrowserRouter>
         <MainNavbar />
         <Routes>
-          <Route exact path='/' Component={Home} />
-          <Route exact path='/about' Component={About} />
-          <Route exact path='/employees/list' Component={EmployeesList} />
-          <Route exact path='/employees/:id' Component={EmployeesDetails} />
-          <Route exact path='/stocks/list' Component={Stocks} />
-          <Route exact path='/stocks/:id' Component={StockDetails} />
-          <Route exact path='/contact' Component={Contact} />
+          
         </Routes>
       </BrowserRouter>
       <FloatingWhatsApp phoneNumber='+917330485402'
--- src/component/layout/MainNavbar.js
+++ src/component/layout/MainNavbar.js
@@ -6,27 +6,13 @@ let MainNavbar = () => {
         <React.Fragment>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <Link to="/" className="navbar-brand">React Hooks Routing</Link>
+                    <Link to="/" className="navbar-brand">React Hooks Bigbasket</Link>
                     <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#main-nav">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="main-nav">
                         <ul className="nav nav-underline">
-                            <li className="nav-item">
-                                <Link to='/' className="nav-link text-white">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/employees/list' className="nav-link text-white">Employees</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/stocks/list' className="nav-link text-white">Stocks</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/about' className="nav-link text-white">About</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/contact' className="nav-link text-white">Contact</Link>
-                            </li>
+                           
                         </ul>
                     </div>
                 </div>
