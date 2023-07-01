74055b4ed60eaa9179617357b85d59cfa5ed185b
musab-omer <mosab.katry@gmail.com>
2023-07-01 20:51:35 +0300

    Configured navigation links

--- src/component/layout/MainNavbar.js
+++ src/component/layout/MainNavbar.js
@@ -6,13 +6,24 @@ let MainNavbar = () => {
         <React.Fragment>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <Link to="/" className="navbar-brand">React Hooks Bigbasket</Link>
+                    <Link to="/" className="navbar-brand">
+                    <i className="bi bi-cart4 mx-1"></i> React Hooks Bigbasket</Link>
                     <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#main-nav">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="main-nav">
-                        <ul className="nav nav-underline">
-                           
+                        <ul className="navbar-nav nav-underline">
+                           <li className="nav-item">
+                            <Link to="/" className="nav-link">Home</Link>
+                           </li>
+                           <li className="nav-item">
+                            <Link to="/products/list" className="nav-link">Product</Link>
+                           </li>
+                        </ul>
+                        <ul className="navbar-nav nav-underline" style={{marginLeft:"auto"}}>
+                        <li className="nav-item">
+                            <Link to="/products/admin" className="nav-link">Admin</Link>
+                           </li>
                         </ul>
                     </div>
                 </div>
