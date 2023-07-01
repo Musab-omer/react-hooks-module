3bef71fff05adff3d7bf1d580c723eea2b42e6df
musab-omer <mosab.katry@gmail.com>
2023-07-01 20:49:55 +0300

    Configure routers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import MainNavbar from './component/layout/MainNavbar';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
+import Home from './component/views/home/Home';
+import ProductsList from './component/views/product/ProductsList';
+import ProductAdmin from './component/views/product/ProductAdmin';
+import CreateProduct from './component/views/product/CreateProduct';
+import UpdateProduct from './component/views/product/UpdateProduct';
 
 
 let App = () => {
@@ -11,7 +16,11 @@ let App = () => {
       <BrowserRouter>
         <MainNavbar />
         <Routes>
-          
+          <Route exact path='/' Component={Home}/>
+          <Route exact path='/products/list' Component={ProductsList}/>
+          <Route exact path='/products/admin' Component={ProductAdmin}/>
+          <Route exact path='products/create' Component={CreateProduct}/>
+          <Route exact path='products/:id' Component={UpdateProduct}/>
         </Routes>
       </BrowserRouter>
       <FloatingWhatsApp phoneNumber='+917330485402'
