719c7cde2854ed84de2c8300053894aea0a06912
musab-omer <mosab.katry@gmail.com>
2023-07-04 16:35:30 +0300

    imported all message component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import MainNavbar from './component/layout/MainNavbar';
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
 import ButtonExample from './component/views/ButtonExample';
 import AcceptTerms from './component/views/AcceptTerms';
+import MessageOne from './component/views/MessageOne';
+import MessageTwo from './component/views/MessageTwo';
+import MessageThree from './component/views/MessageThree';
+import MessageFour from './component/views/MessageFour';
+import MessageFive from './component/views/MessageFive';
 
 
 
@@ -11,7 +16,12 @@ let App = () => {
     <div className="App">
       <MainNavbar />
       {/* <ButtonExample/> */}
-      <AcceptTerms/>
+      {/* <AcceptTerms/> */}
+      <MessageOne/>
+      <MessageTwo/>
+      <MessageThree/>
+      <MessageFour/>
+      <MessageFive/>
       <FloatingWhatsApp phoneNumber='+917330485402'
         accountName='Code With Musab'
         allowEsc
